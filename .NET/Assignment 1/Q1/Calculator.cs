860ef9d2b514ce48dcd5a7169345b736ad93d405
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-21 15:29:05 +0530

    Create Calculator.cs

---
+++ .NET/Assignment 1/Q1/Calculator.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Calculator
+    {
+        internal int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        internal int product(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
