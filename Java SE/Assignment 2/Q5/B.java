4dd96ca331ec2890882a70e3a3c199b390f83347
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-21 17:14:17 +0530

    Create B.java

---
+++ Java SE/Assignment 2/Q5/B.java
@@ -0,0 +1,14 @@
+class B {
+	private static char ch='B';
+	private int num;
+	
+	public static char getCh() {
+		return ch;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+}
