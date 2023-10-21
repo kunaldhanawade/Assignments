84ad71a262e1195a1f5a7f3ec1e6dd33dedb6d9a
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-21 17:05:03 +0530

    Create MyClass.java

---
+++ Java SE/Assignment 2/Q1/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+
+	public static void main(String[] args) {
+		Shape s = new Shape();
+		s.setWidth(20);
+		s.setHeight(30);
+		System.out.println("width: "+s.getWidth()+"\theight: "+s.getHeight());
+
+	}
+
+}
