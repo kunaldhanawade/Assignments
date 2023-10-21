3cc6e745f6f1e3c6abc9ac7757c07113670b42c5
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-21 15:41:25 +0530

    Add Class2.cs and Program.cs

---
+++ .NET/Assignment 1/Q2/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAC
+{
+    internal class Student
+    {
+        internal void add(string name)
+        {
+            Console.WriteLine("Enrolled " + name + " for DAC Course");
+        }
+    }
+}
---
+++ .NET/Assignment 1/Q2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAC;
+using DBDA;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DAC.Student student1 = new DAC.Student();
+            DBDA.Student student2 = new DBDA.Student();
+
+            student1.add("Kunal Dhanawade");
+            student2.add("Mahesh Katale");
+            Console.ReadLine();
+        }
+    }
+}
