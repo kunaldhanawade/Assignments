1c5f26d8b2f431d11229a025e07bba7badd691f0
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-21 15:40:36 +0530

    Create Class1.cs

---
+++ .NET/Assignment 1/Q2/Class1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBDA
+{
+    internal class Student
+    {
+        internal void add(string name)
+        {
+            Console.WriteLine("Enrolled " + name + " for DBDA Course");
+        }
+    }
+}
