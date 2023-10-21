45e1318e35eeb4a34b2cdc2a7e1efe7e12a35567
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-21 16:48:50 +0530

    Create MyClass.java

---
+++ Java SE/Assignment 1/Q7/MyClass.java
@@ -0,0 +1,10 @@
+public class MyClass {
+
+	public static void main(String[] args) {		
+		for (char c='A'; c<='C'; c++) {
+			for (char ch='a'; ch<='c'; ch++)
+				System.out.println(c+"\t"+ch);
+			System.out.println();
+		}
+	}
+}
