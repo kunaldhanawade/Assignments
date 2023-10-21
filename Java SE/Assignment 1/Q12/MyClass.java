3be2e405454aad9b6f9f76466d023a14de783814
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-21 17:02:14 +0530

    Create MyClass.java

---
+++ Java SE/Assignment 1/Q12/MyClass.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter no 1: ");
+		int a = sc.nextInt();
+		System.out.println("Enter no 2: ");
+		int b = sc.nextInt();
+
+		System.out.print("Division: " + (a / b));
+	}
+}
