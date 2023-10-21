341a9e9a442eba90731b59056159a422a5b1cff4
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-21 17:13:43 +0530

    Create C.java

---
+++ Java SE/Assignment 2/Q5/C.java
@@ -0,0 +1,14 @@
+class C {
+	private static char ch='C';
+	private int num;
+	
+	public static char getCh() {
+		return ch;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;    
+	}
+}
