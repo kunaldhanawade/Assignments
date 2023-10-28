1e531328634c384ce27570f3504f3d8405a91d48
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-28 19:41:42 +0530

    Create Second.java

---
+++ Java SE/Assignment 4/Q1/Second.java
@@ -0,0 +1,17 @@
+package package1;
+
+public class Second {
+	protected int num;
+	
+	public Second() {
+		this.num = 247;
+	}
+	
+	public Second(int num) {
+		this.num = num;
+	}
+
+	public int getNum() {
+		return num;
+	}
+}
