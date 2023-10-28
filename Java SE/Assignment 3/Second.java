8db54fe2794576a7fc57376540907677a99433e3
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-28 19:31:49 +0530

    Create Second.java

---
+++ Java SE/Assignment 3/Second.java
@@ -0,0 +1,23 @@
+package devpack;
+
+/**
+ * @author kunal
+ *
+ */
+public class Second {
+private String name;
+
+/**
+ * @return the name
+ */
+public String getName() {
+	return name;
+}
+
+/**
+ * @param name the name to set
+ */
+public void setName(String name) {
+	this.name = name;
+}
+}
