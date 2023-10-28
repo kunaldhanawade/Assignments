5840058770606bb632e7fc1bf70efc33a9aea1c8
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-28 18:50:59 +0530

    Add files via upload

---
+++ .NET/Assignment 7/AccountDemo2/Current.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Xml.Linq;
+
+namespace AccountDemo
+{
+    internal class Current : Account
+    {
+        public Current(string name) : base(name) { }
+        public override void withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
---
+++ .NET/Assignment 7/AccountDemo2/Savings.cs
@@ -0,0 +1,17 @@
+﻿
+namespace AccountDemo
+{
+    internal class Savings : Account
+    {
+        const double minBalance = 1000;
+        public Savings(string name) : base(name) { }
+        public override void withdraw(double amount)
+        {
+            double x = Balance - amount;
+            if ( x >= minBalance)
+            {
+                Balance = x;
+            }
+        }
+    }
+}
