8c591b0e60b8f2316a717fa8ded4b82f8cef8cb6
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-28 19:38:25 +0530

    Create Sub1.java

---
+++ Java SE/Assignment 4/Access Specifiers Assignment/Sub1.java
@@ -0,0 +1,19 @@
+package package1;
+
+public class Base {
+	private void fun1() {
+		System.out.println("in base private fun1");
+	}
+	
+	void fun2() {
+		System.out.println("in base def fun2");
+	}
+	
+	protected void fun3() {
+		System.out.println("in base protected fun3");
+	}
+	
+	public void fun4() {
+		System.out.println("in base public fun4");
+	}
+}
