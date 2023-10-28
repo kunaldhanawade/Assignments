55f5425eb1357410cc7abe71b112eea512f57156
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-28 19:45:18 +0530

    Create CheckDemo.java

---
+++ Java SE/Assignment 4/Q3/CheckDemo.java
@@ -0,0 +1,11 @@
+public class CheckDemo {
+	public static void main(String[] args) {
+		Check c = new Check();
+		// System.out.println(c.name);
+		System.out.println(c.getName());
+		System.out.println(c.age);
+		System.out.println(c.legend);
+		System.out.println(c.initial);
+		c.disp();
+	}
+}
