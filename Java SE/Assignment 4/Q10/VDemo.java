0fbcc4ccc074357cc590811c0c72338597782bf4
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-28 20:00:08 +0530

    Create VDemo.java

---
+++ Java SE/Assignment 4/Q10/VDemo.java
@@ -0,0 +1,24 @@
+public class VDemo {
+	public static void perform(Vehicle ref) {
+		ref.start();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("using perform function");
+		perform(new TwoWheeler());
+		perform(new ThreeWheeler());
+		perform(new FourWheeler());
+		
+		System.out.println("----------------------------");
+		
+		System.out.println("using array of instances");
+		Vehicle v[] = new Vehicle[3];
+		v[0] = new TwoWheeler();
+		v[1] = new ThreeWheeler();
+		v[2] = new FourWheeler();
+		
+		for (int i=0; i<v.length; i++) {
+			v[i].start();
+		}
+	}
+}
