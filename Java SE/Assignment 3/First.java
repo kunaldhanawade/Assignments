cb885aaeb51f9e67524b800d0261bac1ee0858ef
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-28 19:30:58 +0530

    Create First.java

---
+++ Java SE/Assignment 3/First.java
@@ -0,0 +1,26 @@
+package devpack;
+
+public class First {
+private int num;
+
+/**
+ * @param x 1 param constr
+ */
+public First(int x) {
+	this.num = x;
+}
+
+/**
+ * @return the num
+ */
+public int getNum() {
+	return num;
+}
+
+/**
+ * @param num the num to set
+ */
+public void setNum(int num) {
+	this.num = num;
+}
+}
