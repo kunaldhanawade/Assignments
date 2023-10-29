9157306cb27b20f39229a21b897b316e6c925757
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 16:14:30 +0530

    Create GUIDemo.java

---
+++ Java SE/Assignment 6/Q2/GUIDemo.java
@@ -0,0 +1,13 @@
+public class GUIDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WindowGUIApp gui = new WindowGUIApp();
+		gui.mouseClicked();
+		gui.mousePressed();
+		gui.mouseReleased();
+		gui.windowClosing();
+		gui.windowClosed();
+	}
+
+}
