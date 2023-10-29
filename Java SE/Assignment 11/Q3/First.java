167dca96285ce070d154441cb602c95ce51c6838
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 20:10:57 +0530

    Create First.java

---
+++ Java SE/Assignment 11/Q3/First.java
@@ -0,0 +1,12 @@
+public class First
+{
+	public static void main(String args[]) {
+		ExecutorService exse = Executors.newFixedThreadPool(2);
+		
+		MyApp myapp = new MyApp();
+		
+		exse.execute(myapp);
+		exse.execute(myapp);
+		exse.shutdown();
+	}
+}
