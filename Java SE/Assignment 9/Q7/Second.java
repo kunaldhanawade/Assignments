0f523c769d982c3ea629931f4eb63d12c1c9c560
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 20:00:31 +0530

    Create Second.java

---
+++ Java SE/Assignment 9/Q7/Second.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package devpack;
+
+/**
+ * @author kunal
+ * 
+ */
+public class Second {
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "[ "+this.getClass().getName()+" ]";
+	}
+}
+
