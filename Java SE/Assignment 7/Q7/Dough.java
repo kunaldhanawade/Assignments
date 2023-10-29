cce9fcef77db9fe7f9d54220ccb8f29eee8ed680
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 16:53:54 +0530

    Create Dough.java

---
+++ Java SE/Assignment 7/Q7/Dough.java
@@ -0,0 +1,13 @@
+public class Dough {
+	private String dough;
+
+	public Dough(String dough) {
+		this.dough = dough;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "["+dough+"]";
+	}
+}
