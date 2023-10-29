6235a5e35bbee58198acb1ca823f5f97b16be17c
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 16:45:22 +0530

    Create Rectangle.java

---
+++ Java SE/Assignment 7/Q7/Rectangle.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package devpack;
+
+/**
+ * @author Kunal
+ *
+ */
+public class Rectangle implements Shape {
+	public void draw() {
+		System.out.println("drawing rectangle");
+	}
+}
+
