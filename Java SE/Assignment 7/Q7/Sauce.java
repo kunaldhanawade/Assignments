ea8db463461cbe45aca85b2f6232bff23e25b0e3
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 16:55:08 +0530

    Create Sauce.java

---
+++ Java SE/Assignment 7/Q7/Sauce.java
@@ -0,0 +1,13 @@
+public class Sauce {
+	private String sauce;
+	
+	public Sauce(String sauce){
+		this.sauce=sauce;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "["+sauce+"]";
+	}
+}
