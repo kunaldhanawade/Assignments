0851edff1a138ede02863f47b586fc40060ea008
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 19:40:33 +0530

    Create DACDBDACourse.java

---
+++ Java SE/Assignment 7/Q9/DACDBDACourse.java
@@ -0,0 +1,16 @@
+public class DACDBDACourse implements Course {
+
+	public void start()
+	{
+		System.out.println("course started");
+	}
+	
+	public void stop()
+	{
+		System.out.println("course stopped");
+	}
+	
+	public void orientation () {
+		System.out.println("orientation");
+	}
+}
