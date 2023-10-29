4b0f09dd68495bd6c0f0ef18d5bec1edc7139259
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 19:56:53 +0530

    Create Musician.java

---
+++ Java SE/Assignment 9/Q4/Musician.java
@@ -0,0 +1,9 @@
+public class Musician extends Artist {
+	@Override
+	public void perform() {
+		// TODO Auto-generated method stub
+		super.perform();
+		System.out.println("amazing beats");
+	}
+}
+
