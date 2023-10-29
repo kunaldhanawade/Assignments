7289351d6a5adb6e30fb3fc5dbd6d9deea78a4fe
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 16:38:06 +0530

    Create ShapeDemo.java

---
+++ Java SE/Assignment 7/Q3/ShapeDemo.java
@@ -0,0 +1,18 @@
+public class ShapeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shape s[] = new Shape[3];
+
+		s[0] = new Triangle();
+		s[1] = new Rectangle();
+		s[2] = new Circle();
+		
+		for(int i=0; i<s.length; i++) {
+			s[i].draw();
+			s[i].show();
+			System.out.println();
+		}
+	}
+
+}
