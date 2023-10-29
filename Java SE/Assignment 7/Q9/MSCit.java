4cbd5676304eb95a8bbecf88b7a808ceb5745948
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 19:41:03 +0530

    Create MSCit.java

---
+++ Java SE/Assignment 7/Q9/MSCit.java
@@ -0,0 +1,13 @@
+public class MSCit implements Course {
+
+	public void start()
+	{
+		System.out.println("course started");
+	}
+	
+	public void stop()
+	{
+		System.out.println("course stopped");
+	}
+
+}
