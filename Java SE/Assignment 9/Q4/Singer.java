7ad7982dffd9730a7bde7bb0223fea0418b1bc49
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 19:56:30 +0530

    Create Singer.java

---
+++ Java SE/Assignment 9/Q4/Singer.java
@@ -0,0 +1,9 @@
+public class Singer extends Artist {
+	@Override
+	public void perform() {
+		// TODO Auto-generated method stub
+		super.perform();
+		System.out.println("pleasent voice");
+	}
+}
+
