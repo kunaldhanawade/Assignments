763d5ae8c0edebf7a2facf1f71462a19009c53a1
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 16:43:34 +0530

    Create CommandLineDemo.java

---
+++ Java SE/Assignment 7/Q5/CommandLineDemo.java
@@ -0,0 +1,12 @@
+public class CommandLineDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum=0;
+		for(int i=0;i<args.length;i++)
+			sum+=Integer.parseInt(args[i]);
+		System.out.println(sum);
+	}
+
+}
