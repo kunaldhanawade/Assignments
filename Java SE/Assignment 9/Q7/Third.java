9a34c60fa9de5208ea17e1aa170107ff54ff0f14
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 20:01:00 +0530

    Create Third.java

---
+++ Java SE/Assignment 9/Q7/Third.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package devpack;
+
+/**
+ * @author kunal
+ * 
+ */
+public class Third {
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "[ "+this.getClass().getName()+" ]";
+	}
+}
+
