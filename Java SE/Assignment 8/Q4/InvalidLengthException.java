3e64dd18fd2975588d8a93aeee837724034f88a2
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 19:48:34 +0530

    Create InvalidLengthException.java

---
+++ Java SE/Assignment 8/Q4/InvalidLengthException.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package devpack;
+
+/**
+ *  @author Kunal
+ * 
+ */
+public class InvalidLengthException extends Exception {
+	
+	public InvalidLengthException() {
+		super("Invalid Length Exception");
+	}
+}
