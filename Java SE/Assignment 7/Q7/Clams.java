62e98090fc025ad254741d957f08f5ddb0692c49
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 16:56:41 +0530

    Create Clams.java

---
+++ Java SE/Assignment 7/Q7/Clams.java
@@ -0,0 +1,13 @@
+public class Clams {
+	private String clams;
+
+	public Clams(String clams) {
+		this.clams = clams;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "["+clams+"]";
+	}
+}
