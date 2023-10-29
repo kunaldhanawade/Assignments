a61dd2b8f6b147ea52001789864cd02c078039d3
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 20:01:25 +0530

    Create Fourth.java

---
+++ Java SE/Assignment 9/Q7/Fourth.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package devpack;
+
+/**
+ * 
+ */
+public class Fourth {
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "[ "+this.getClass().getName()+" ]";
+	}
+}
