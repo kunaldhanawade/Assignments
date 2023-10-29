8abaa2bbb05a781369a9bf19f8cb3e760e69320f
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 16:56:13 +0530

    Create Pepperoni.java

---
+++ Java SE/Assignment 7/Q7/Pepperoni.java
@@ -0,0 +1,13 @@
+public class Pepperoni {
+	private String pepperoni;
+
+	public Pepperoni(String pepperoni) {
+		this.pepperoni = pepperoni;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "["+pepperoni+"]";
+	}
+}
