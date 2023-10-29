506a6263d4cdb19e736f8223565e3e067e25471c
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 19:46:02 +0530

    Create NumberNotDivisibleBySevenException.java

---
+++ Java SE/Assignment 8/Q2/NumberNotDivisibleBySevenException.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package devpack;
+
+/**
+ *  @author Kunal
+ * 
+ */
+public class NumberNotDivisibleBySevenException extends Exception {
+	
+	/**
+	 * @param e
+	 */
+	public NumberNotDivisibleBySevenException() {
+		super("Number Not Divisible By Seven Exception");
+	}
+}
