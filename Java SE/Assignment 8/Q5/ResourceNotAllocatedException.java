1f9a5090b2f8f247515d7b5b6de3d360fda70723
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 19:49:55 +0530

    Create ResourceNotAllocatedException.java

---
+++ Java SE/Assignment 8/Q5/ResourceNotAllocatedException.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package devpack;
+
+/**
+ *  @author Kunal
+ * 
+ */
+public class ResourceNotAllocatedException extends Exception {
+	
+	public ResourceNotAllocatedException(String message) {
+		super(message);
+	}
+}
