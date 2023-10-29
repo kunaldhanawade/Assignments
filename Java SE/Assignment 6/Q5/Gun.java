2f29e228a8557b5d9fe8abb1bbea57a69acf8022
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 16:27:29 +0530

    Create Gun.java

---
+++ Java SE/Assignment 6/Q5/Gun.java
@@ -0,0 +1,11 @@
+package weaponpack;
+
+public class Gun extends Weapon {
+	void attack() {
+		System.out.println("using gun to attack");
+	}
+	
+	void fillBullets() {
+		System.out.println("filling bullets");
+	}
+}
