f2fd0afc3d1d5761154490ce697ca5b1b62ea73e
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 19:56:09 +0530

    Create Actor.java

---
+++ Java SE/Assignment 9/Q4/Actor.java
@@ -0,0 +1,15 @@
+public class Actor extends Artist {
+	@Override
+	public void perform() {
+		// TODO Auto-generated method stub
+		super.perform();
+		System.out.println("excellent acting");
+	}
+	
+	void changeGetUp()
+	{
+		System.out.println("ghanging getup as per the role demands");
+	}
+
+}
+
