53feb738934e0fcdd864d3eeb193171e92b9b25c
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-29 16:55:43 +0530

    Create Cheese.java

---
+++ Java SE/Assignment 7/Q7/Cheese.java
@@ -0,0 +1,13 @@
+public class Cheese {
+	private String cheese;
+	
+	public Cheese(String cheese){
+		this.cheese=cheese;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "["+cheese+"]";
+	}
+}
