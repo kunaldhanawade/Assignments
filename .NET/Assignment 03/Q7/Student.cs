b2a3de827c1f054aa770c37f95642d5aa52cc7c3
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-10-31 19:55:40 +0530

    Create Student.cs

---
+++ .NET/Assignment 03/Q7/Student.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp
+{
+    internal class Student
+    {
+        private string name;
+        private int maths;
+        private int science;
+        private int english;
+        private int total;
+
+        static Student()
+        {
+            Console.WriteLine("Holy Cross High School");
+        }
+
+        public Student(string name, int maths, int science, int english)
+        {
+            this.name = name;
+            if (maths <= 100 && science <= 100 && english <= 100)
+            {
+                this.maths = maths;
+                this.science = science;
+                this.english = english;
+            }
+            else
+            {
+                throw new Exception("Invalid Marks");
+            }
+        }
+
+        public int get_Total()
+        {
+            if (total == 0)
+                total = maths + science + english;
+            return total;
+        }
+
+        public string display()
+        {
+            return $"Name: {name}\tTotal Marks: {get_Total()}";
+        }
+
+        public static void Give_marks(Student s)
+        {
+            s.maths = s.maths + 5;
+        }
+    }
+}
