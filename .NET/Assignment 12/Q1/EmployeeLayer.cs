2a9e2075dd1a535f67a6d24afdffb74961d0a48a
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-11-03 17:12:21 +0530

    Add files via upload

---
+++ .NET/Assignment 12/Q1/EmployeeLayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace MyConsoleApp
+{
+    public class EmployeeLayer
+    {
+        string connectionString;
+
+        public EmployeeLayer(IConfiguration iconfig)
+        {
+            connectionString = iconfig.GetConnectionString("Default");
+        }
+
+        public void Employees()
+        {
+            using(SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("Select * From Employee_Data", con);
+                con.Open();
+                Console.WriteLine("connected");
+                SqlDataReader data = cmd.ExecuteReader();
+
+                while(data.HasRows)
+                {
+                    while(data.Read())
+                    {
+                        Console.WriteLine($"{data["id"]} \t {data["Name"]} \t {data["Salary"]}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ .NET/Assignment 12/Q1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static IConfiguration _iconfig;
+        static void GetAppsettingsFile()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("Appsettings.json", optional: false, reloadOnChange: true);
+            _iconfig = builder.Build();
+        }
+
+        static void PrintEmployee()
+        {
+            EmployeeLayer emp = new EmployeeLayer(_iconfig);
+            emp.Employees();
+        }
+        static void Main(string[] args)
+        {
+            GetAppsettingsFile();
+            PrintEmployee();
+        }
+    }
+}
