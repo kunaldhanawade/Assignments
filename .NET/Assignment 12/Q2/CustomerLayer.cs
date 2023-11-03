c613b39d0177f1d2e6e8432e2c77d6476e0cf20f
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-11-03 20:22:34 +0530

    Add files via upload

---
+++ .NET/Assignment 12/Q2/CustomerLayer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace MyConsoleApp
+{
+    public class CustomerLayer
+    {
+        string connectionString;
+        public CustomerLayer(IConfiguration iconfig)
+        {
+            connectionString = iconfig.GetConnectionString("Default");
+        }
+
+        public void AddCustomers(string name, string address, long mobno)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand($"Insert into Customer values ('{name}', '{address}', {mobno})", con);
+                    con.Open();
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected != 0)
+                    {
+                        Console.WriteLine("Data Inserted Successfully... "+rowsAffected+" Row Affected");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public void UpdateCustomer(int id, string name)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand($"Update Customer Set Name='{name}' Where Id={id}", con);
+                    con.Open();
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected != 0)
+                    {
+                        Console.WriteLine("Data Updated Successfully... "+rowsAffected+" Row Affected");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+
+
+        public void DeleteCustomer(int id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand($"Delete From Customer Where Id={id}", con);
+                    con.Open();
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected != 0)
+                    {
+                        Console.WriteLine("Data Deleted Successfully... "+rowsAffected +" Row Affected");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public void ShowCustomers()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("Select * from Customer", con);
+
+                    con.Open();
+
+                    SqlDataReader data = cmd.ExecuteReader();
+
+                    if (data.HasRows)
+                    {
+                        while (data.Read())
+                        {
+                            Console.WriteLine($"{data["id"]}\t{data["Name"]}\t{data["Address"]}\t{data["MobileNumber"]}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ .NET/Assignment 12/Q2/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static IConfiguration _iconfig;
+        static void GetAppsettingsFile()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("Appsettings.json", optional: false, reloadOnChange: true);
+            _iconfig = builder.Build();
+        }
+
+        static void InsertCustomer(string name, string address, long mobno)
+        {
+            string values = $"('{name}', '{address}', {mobno})";
+            CustomerLayer customer = new CustomerLayer(_iconfig);
+            customer.AddCustomers(name, address, mobno);
+        }
+
+        static void ChangeNameUsingID(int id, string name)
+        {
+            CustomerLayer customer = new CustomerLayer(_iconfig);
+            customer.UpdateCustomer(id, name);
+        }
+
+        static void DeleteCustomer(int id)
+        {
+            CustomerLayer customer = new CustomerLayer(_iconfig);
+            customer.DeleteCustomer(id);
+        }
+
+        static void PrintCustomer()
+        {
+            CustomerLayer customer = new CustomerLayer(_iconfig);
+            customer.ShowCustomers();
+        }
+        static void Main(string[] args)
+        {
+            GetAppsettingsFile();
+            InsertCustomer("Harsh", "Goregaon", 9576570188);
+            InsertCustomer("Omkar", "Lalbaug", 9565701245);
+            InsertCustomer("Vedant", "Amravati", 7012345765);
+            InsertCustomer("Akshay", "Cotton", 8657013747);
+            PrintCustomer();
+            ChangeNameUsingID(1, "Kunal");
+            PrintCustomer();
+            DeleteCustomer(3);
+            PrintCustomer();
+        }
+    }
+}
