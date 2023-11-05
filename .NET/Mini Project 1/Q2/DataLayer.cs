813f32edbd221bba73104001926daaf184d0865d
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-11-05 23:59:39 +0530

    Add files via upload

---
+++ .NET/Mini Project 1/Q2/DataLayer.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SqlServer.Server;
+
+namespace MiniProjectApp
+{
+    internal class DataLayer
+    {
+        string connectionSring;
+        public DataLayer(IConfiguration iconfig)
+        {
+            connectionSring = iconfig.GetConnectionString("Default");
+        }
+
+        SqlConnection GetSqlConnection()
+        {
+            return new SqlConnection(connectionSring);
+        }
+        public void Add(Employee e)
+        {
+            using (SqlConnection conn = GetSqlConnection())
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "INSERT INTO EMPLOYEE (Name, Salary, Gender, Address) values (@eName, @eSalary, @eGender, @eAddress)";
+                    cmd.Parameters.Add("@eName", System.Data.SqlDbType.NVarChar).Value = e.Name;
+                    cmd.Parameters.Add("@eSalary", System.Data.SqlDbType.Float).Value = e.Salary;
+                    cmd.Parameters.Add("@eGender", System.Data.SqlDbType.Char).Value = e.Gender;
+                    cmd.Parameters.Add("@eAddress", System.Data.SqlDbType.NVarChar).Value = e.Address;
+                    cmd.Connection = conn;
+
+                    conn.Open();
+                    int record = cmd.ExecuteNonQuery();
+                    Console.WriteLine("Inserted " + record);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+        public void Delete(int Id)
+        {
+            using (SqlConnection conn = GetSqlConnection())
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "DELETE FROM EMPLOYEE WHERE Id=@eId";
+                    cmd.Parameters.Add("@eId", System.Data.SqlDbType.NVarChar).Value = Id;
+                    cmd.Connection = conn;
+
+                    conn.Open();
+                    int record = cmd.ExecuteNonQuery();
+                    Console.WriteLine("Deleted " + record);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+        public void Update(Employee e, int Id)
+        {
+            using (SqlConnection conn = GetSqlConnection())
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "UPDATE EMPLOYEE SET Name=@eName, Salary=@eSalary, Gender=@eGender, Address=@eAddress WHERE Id=@eId";
+                    cmd.Parameters.Add("@eId", System.Data.SqlDbType.NVarChar).Value = Id;
+                    cmd.Parameters.Add("@eName", System.Data.SqlDbType.NVarChar).Value = e.Name;
+                    cmd.Parameters.Add("@eSalary", System.Data.SqlDbType.Float).Value = e.Salary;
+                    cmd.Parameters.Add("@eGender", System.Data.SqlDbType.Char).Value = e.Gender;
+                    cmd.Parameters.Add("@eAddress", System.Data.SqlDbType.NVarChar).Value = e.Address;
+                    cmd.Connection = conn;
+
+                    conn.Open();
+                    int record = cmd.ExecuteNonQuery();
+                    Console.WriteLine("Updated " + record);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+        public Employee Get(int id)
+        {
+            Employee e = null;
+            using (SqlConnection conn = GetSqlConnection())
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "SELECT * FROM EMPLOYEE WHERE Id=@eId";
+                    cmd.Parameters.Add("@eId", System.Data.SqlDbType.NVarChar).Value = id;
+                    cmd.Connection = conn;
+
+                    conn.Open();
+                    SqlDataReader data = cmd.ExecuteReader();
+                    if (data.HasRows)
+                    {
+                        while (data.Read())
+                            e = new Employee
+                            {
+                                Id = Convert.ToInt32(data["Id"]),
+                                Name = data["Name"].ToString(),
+                                Salary = Convert.ToDouble(data["Salary"]),
+                                Gender = data["Gender"].ToString()[0],
+                                Address = data["Address"].ToString()
+                            };
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+            return e;
+        }
+        public List<Employee> GetAll()
+        {
+            List<Employee> list = new List<Employee>();
+            using (SqlConnection conn = GetSqlConnection())
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "SELECT * FROM EMPLOYEE";
+                    cmd.Connection = conn;
+
+                    conn.Open();
+                    SqlDataReader data = cmd.ExecuteReader();
+                    if (data.HasRows)
+                    {
+                        while (data.Read())
+                            list.Add(new Employee
+                            {
+                                Id = Convert.ToInt32(data["Id"]),
+                                Name = data["Name"].ToString(),
+                                Salary = Convert.ToDouble(data["Salary"]),
+                                Gender = data["Gender"].ToString()[0],
+                                Address = data["Address"].ToString()
+                            });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ .NET/Mini Project 1/Q2/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MiniProjectApp
+{
+    internal class Program
+    {
+        static IConfiguration _iconfig;
+        static DataLayer dl;
+        static void Main(string[] args)
+        {
+            GetAppSettingsFile();
+            WorkWithEmployee();
+        }
+
+        static void GetAppSettingsFile()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("Appsettings.json", optional: false, reloadOnChange: true);
+            _iconfig = builder.Build();
+        }
+
+        static void WorkWithEmployee()
+        {
+            dl = new DataLayer(_iconfig);
+
+            AddEmployee("Kunal", 75000, 'M', "Prabhadevi");
+
+            GetAllEmployees();
+
+            //UpdateEmployee(8, "Phalesh", 30000, 'M', "Dombivli W");
+
+            //Employee e = dl.Get(8);
+            //Console.WriteLine($"ID: {e.Id} Name: {e.Name} Salary: {e.Salary} Gender: {e.Gender} Address: {e.Address}");
+
+            dl.Delete(9);
+
+            GetAllEmployees();
+        }
+
+        static void AddEmployee(string name, double salary, char gender, string address)
+        {
+            dl.Add(new Employee()
+            {
+                Name = name,
+                Salary = salary,
+                Gender = gender,
+                Address = address
+            });
+        }
+
+        static void GetAllEmployees()
+        {
+            List<Employee> employeeList = dl.GetAll();
+            Console.WriteLine("ID\tName\tSalary\tGender\tAddress");
+            foreach (Employee emp in employeeList)
+            {
+                Console.WriteLine($"{emp.Id}\t{emp.Name}\t{emp.Salary}\t{emp.Gender}\t{emp.Address}");
+            }
+        }
+
+        static void UpdateEmployee(int id, string name, double salary, char gender, string address)
+        {
+            Employee temp = new Employee()
+            {
+                Name = name,
+                Salary = salary,
+                Gender = gender,
+                Address = address
+            };
+            dl.Update(temp, id);
+        }
+    }
+}
