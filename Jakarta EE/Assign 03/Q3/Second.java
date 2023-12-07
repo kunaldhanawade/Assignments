167e6bc10e6b3340f7bb8b39d0add94ccdf9eea4
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-12-07 19:50:18 +0530

    Add files via upload

---
+++ Jakarta EE/Assign 03/Q3/Second.java
@@ -0,0 +1,28 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Second
+ */
+public class Second extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String contextparam = getServletContext().getInitParameter("group");
+		out.println("Context Param - Group: "+contextparam);
+	}
+
+}
