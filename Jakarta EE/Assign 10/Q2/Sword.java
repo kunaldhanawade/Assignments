bfb84dafdfc2140527f0bee26ec8c32cd811a25b
Kunal Dhanawade <kunaldhanawade7@gmail.com>
2023-12-12 23:37:54 +0530

    Add files via upload

---
+++ Jakarta EE/Assign 10/Q2/Sword.java
@@ -0,0 +1,10 @@
+package mypack;
+
+public class Sword implements Weapon {
+
+	@Override
+	public void attack() {
+		System.out.println("Sword attack");
+	}
+
+}
---
+++ Jakarta EE/Assign 10/Q2/Weapon.java
@@ -0,0 +1,5 @@
+package mypack;
+
+public interface Weapon {
+	void attack();
+}
